97f50b4d6bd77adfa64d11c2f2263574a393b5d1
Lena Le <ll224ve@student.lnu.se>
2025-09-04 19:32:08 +0200

    Add styling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const output = document.getElementById('output')
 btn.addEventListener('click', () => {
     const nameInput = document.getElementById('nameInput').value
     const reversedName = reverseName(nameInput)
-    output.textContent = `Hello, ${nameInput}! 🫡 Your name reversed is ${reversedName}.`
+    output.textContent = `Hello, ${nameInput}! 🤍 Your name reversed is ${reversedName}...`
 })
 
 // Takes a string as input and returns the reversed string
